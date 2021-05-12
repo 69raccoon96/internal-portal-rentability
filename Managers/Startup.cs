555c80f0dc4dbabee93d3dd8f903b203a9290dc4
69raccoon96 <raccoon.twitch@gmail.com>
2021-05-13 00:13:33 +0500

    fix api

--- Managers/Startup.cs
+++ Managers/Startup.cs
@@ -19,10 +19,7 @@ namespace Managers
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(c =>  
-            {  
-                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());  
-            });
+            services.AddCors(c => { c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin()); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,10 +29,12 @@ namespace Managers
             {
                 app.UseDeveloperExceptionPage();
             }
+
             //TODO раскидать эндпоинты по файлам
             app.UseRouting();
-            app.UseCors(options => options.AllowAnyOrigin());  
+            app.UseCors(options => options.AllowAnyOrigin());
             var db = new DataBase();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
@@ -55,12 +54,18 @@ namespace Managers
                         await context.Response.WriteAsync(JsonConvert.SerializeObject(db.GetUsers()));
                     });
                 endpoints.MapGet("/projects",
-                    async context =>
-                    {
-                        await context.Response.WriteAsync(JsonConvert.SerializeObject(db.GetProjects(context.Request)));
-                    });
-
+                    async context => { await context.Response.WriteAsync(SomeMethod(context, db)); });
             });
+        }
+
+        private string SomeMethod(HttpContext context, DataBase db)
+        {
+            var data = db.GetProjects(context.Request).First();
+            var jsonResolver = new PropertyRenameAndIgnoreSerializerContractResolver();
+            jsonResolver.IgnoreProperty(typeof(Project), "FirstName", "LastName", "OverdueTime", "OverdueTime",
+                "DateStart", "DateEnd", "OverdueTasks");
+            var serializerSettings = new JsonSerializerSettings {ContractResolver = jsonResolver};
+            return JsonConvert.SerializeObject(data, serializerSettings);
         }
     }
 }
