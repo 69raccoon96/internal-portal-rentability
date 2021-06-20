64a7cfb41f9501837cef3ce5a78cfe65ec2a0e62
Dmitry Vasyasin <vasyasin.dmitrii@gmail.com>
2021-06-20 02:35:56 +0000

    double fix

--- ManagersApi/Entities/AnalyticSubparagraph.cs
+++ ManagersApi/Entities/AnalyticSubparagraph.cs
@@ -1,10 +1,10 @@
-﻿namespace ManagersApi.Entities
+namespace ManagersApi.Entities
 {
     public class AnalyticSubparagraph//TODO возможно стоит использовать структуру
     {
         public string Name { get; set; }
-        public int TimePlaned { get; set; }
-        public int TimeSpent { get; set; }
+        public double TimePlaned { get; set; }
+        public double TimeSpent { get; set; }
         
         public string ProjectName { get; set; }
 
@@ -12,7 +12,7 @@
         {
             
         }
-        public AnalyticSubparagraph(string name, int timePlaned, int timeSpent, string projectName)
+        public AnalyticSubparagraph(string name, double timePlaned, double timeSpent, string projectName)
         {
             TimePlaned = timePlaned;
             TimeSpent = timeSpent;
