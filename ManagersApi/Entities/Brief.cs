b6950abce9fab6f42389465ee0ec2a19062ef8d8
Dmitry Vasyasin <vasyasin.dmitrii@gmail.com>
2021-06-20 02:36:19 +0000

    double fix

--- ManagersApi/Entities/Brief.cs
+++ ManagersApi/Entities/Brief.cs
@@ -1,9 +1,9 @@
-﻿namespace ManagersApi.Entities
+namespace ManagersApi.Entities
 {
     public class Brief
     {
         public string FullName { get; set; }
-        public int OverdueTime { get; set; }
+        public double OverdueTime { get; set; }
         public int ProjectsCount { get; set; }
     }
 }
